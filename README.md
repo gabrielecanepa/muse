9e48e81e806f38cf941b9750d96d5e013cdf4759
Gabriele Canepa <gabriele@canepa.io>
2019-01-31 16:38:11 +0000

    Initialize new repository with webpack-init

---
+++ README.md
@@ -0,0 +1,1 @@
+Application generated with [webpack-init](https://github.com/gabrielecanepa/webpack-init), created by [Gabriele Canepa](https://github.com/gabrielecanepa).
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const packageJSON = require('./package.json');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: devMode ? 'development' : 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: '!!html-loader!templates/index.html',
+      favicon: './assets/images/favicon.png',
+      meta: {
+        description: packageJSON.description
+      }
+    })
+  ],
+  devtool: 'sourcemap',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  }
+};
